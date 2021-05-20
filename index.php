636f9c4485cd52ac42ffee705000aa5c6b2c3aa5
Sanjay Palial <sanjay.palial@gmail.com>
2021-05-20 20:14:16 +0530

    test

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $app = require "./core/app.php";
 // Get all users from DB, eager load all fields using '*'
 $users = User::find($app->db,'*');
 
-// Render view 'views/index.php' and pass users variable there
+// Render view 'views/index.php' and pass users variable there1
 $app->renderView('index', array(
 	'users' => $users
 ));
